cc04fe078a27036f5f43369704fd6a5a1b98a949
John Doe <johndoe@example.com>
2024-10-13 09:18:06 +0900

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -89,4 +89,54 @@ jQuery(function ($) {
       pagetop.fadeOut();
     }
   });
+
+  // クリックして開閉するトグル要素を取得
+  const toggleItems = document.querySelectorAll(".js-archive__toggle");
+
+  // 各トグルアイテムにクリックイベントを追加
+  toggleItems.forEach((item) => {
+    item.addEventListener("click", function (event) {
+      event.preventDefault(); // デフォルトのリンク動作を無効化
+
+      // クリックされたアイテムにクラス 'open' を切り替える（矢印の向き）
+      item.classList.toggle("open");
+
+      // 月別項目を取得
+      const months = item.nextElementSibling; // クリックされた次の兄弟要素
+
+      // 月別アイテムが存在し、'aside__archive-item--month' の場合に処理
+      if (months && months.classList.contains("aside__archive-item--month")) {
+        let currentItem = months;
+
+        // 次の月別アイテムが続く限り、'show' クラスのトグル処理
+        while (
+          currentItem &&
+          currentItem.classList.contains("aside__archive-item--month")
+        ) {
+          currentItem.classList.toggle("show");
+          currentItem = currentItem.nextElementSibling;
+        }
+      }
+    });
+  });
+
+  // 全てのpage-faq__questionを取得
+  const faqQuestions = document.querySelectorAll(".js-faq__open");
+
+  // 各質問にクリックイベントを追加
+  faqQuestions.forEach(function (question) {
+    question.addEventListener("click", function () {
+      // クリックされた質問の次の要素（page-faq__answer）を取得
+      const answer = this.nextElementSibling;
+
+      // 答えの開閉状態をトグル
+      if (answer.classList.contains("open")) {
+        answer.classList.remove("open");
+        this.classList.remove("open"); // 質問のopenクラスを削除
+      } else {
+        answer.classList.add("open");
+        this.classList.add("open"); // 質問にopenクラスを追加して「＋」に変更
+      }
+    });
+  });
 });
